8419f41005c1c33997173d15bd97e2791e714980
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2021-10-15 10:13:46 +0500

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main()
+        {
+            Cat cat = new Cat();
+            cat.ShowInfo();
+            cat.Say();
+
+            Console.WriteLine();
+
+            Dog dog = new Dog();
+            dog.ShowInfo();
+            dog.Say();
+
+            Console.ReadKey();
+        }
+
+        abstract class Animal
+        {
+            public string Name { get; set; }
+            public string Voice { get;  set; }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine(Name);
+                Say();
+            }
+
+            public void Say()
+            {
+                Console.WriteLine(Voice);
+            }
+        }
+
+        private class Cat : Animal
+        {
+            public Cat()
+            {
+                Name = "Кошка";
+                Voice = "Мяу";
+            }
+            public new void Say()
+            {
+                Console.WriteLine("Мяу!!!");
+            }
+        }
+
+        private class Dog : Animal
+        {
+            public Dog()
+            {
+                Name = "Собака";
+                Voice = "Гав";
+            }
+            public new void Say()
+            {
+                Console.WriteLine("Гав!!!");
+            }
+        }
+
+    }
+}
